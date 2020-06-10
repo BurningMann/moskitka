7cb8b286b7aeee4ddac508296fde28dd4a2ded7f
burtest <sannnnna@bk.ru>
2020-06-10 15:18:24 +0300

    end

---
+++ js/custom.js
@@ -0,0 +1,38 @@
+window.onload = function(){
+/* Вывод форм, кнопке формы нужно указывать атрибут "data-to" с названием id формы и класс "common_btn" */
+  $(".common_btn, .link_box .more").click(function(EO){
+    let target = '#'+$(this).data("to")
+    $("body").addClass("hidden")
+    $(".modal_bg").fadeIn().css("display","flex");
+    $(target).css("display","flex")
+    if($(this).data("to") == "zakaz"){
+      let name = $(this).closest(".cart").find(".name").text()
+      $(target).find(".notification").text(name) 
+    }
+    $("body").click(function(EO){
+        if(EO.target == $(".modal_bg")[0]){
+            $(".modal_bg").fadeOut()
+            $("body").removeClass("hidden")
+            $(target).css("display","none")
+        }
+    })
+    $(".modal_close").click(function(){
+      $(this).closest(".modal_bg").fadeOut()
+      $("body").removeClass("hidden")
+      $(this).closest(".modal_box").css("display", "none")
+    })
+  })
+  $(".modal_box.more .main_slider").slick({
+    prevArrow: '<div class="slider_btn prev"><img src="../img/icon/slider_arrow.svg"></div>',
+    nextArrow: '<div class="slider_btn next"><img src="../img/icon/slider_arrow.svg"></div>',
+    asNavFor: '.modal_box.more .additional_slider'
+  })
+
+  $(".modal_box.more .additional_slider").slick({
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    arrows: false,
+    focusOnSelect: true,
+    asNavFor: '.modal_box.more .main_slider'
+  })
+}
